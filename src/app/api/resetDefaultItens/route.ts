1ca1b3e26b06c14fab9df715f3cde3a8a7afa953
matwbug <matthewbugado@gmail.com>
2024-03-06 23:50:03 -0300

    Ultimas atualizações do prisma para funcionamento

--- src/app/api/resetDefaultItens/route.ts
+++ src/app/api/resetDefaultItens/route.ts
@@ -10,30 +10,31 @@ import prisma from '@/lib/prisma'
 // Função para lidar com a requisição POST
 export async function POST(req: NextRequest){
     try{
+
         await prisma.produtos.deleteMany({})
 
-        const data = await Promise.all(produtos.map(async(item) => {
-            return await prisma.produtos.create({
-                data: {
-                    name: item.name,
-                    description: item.description,
-                    preco: item.preco,
-                    imageUrl: item.image,
-                    quantidade: item.quantidade,
-                    promocao: item.promocao,
-                    atacado: item.atacado,
-                    atacado_minquantidade: item.atacado_minquantidade,
-                    promocao_preco: item.promocao_preco,
-                    slug: item.slug,
-                    status: ['ATIVO', 'DESATIVADO'].includes(item.status) ? item.status as ProdutoStatus : 'ATIVO',
-                    vendas: item.vendas,
-                },
-                select: {
-                    id: true,
-                    name: true
-                }
-            })
-        }))
+        const data = await Promise.all(produtos.map(async (item) => {
+            return prisma.produtos.create({
+              data: {
+                name: item.name,
+                description: item.description,
+                preco: item.preco,
+                imageUrl: item.image,
+                quantidade: item.quantidade,
+                promocao: item.promocao,
+                atacado: item.atacado,
+                atacado_minquantidade: item.atacado_minquantidade,
+                promocao_preco: item.promocao_preco,
+                slug: item.slug,
+                status: ['ATIVO', 'DESATIVADO'].includes(item.status) ? item.status as ProdutoStatus : 'ATIVO',
+                vendas: item.vendas,
+              },
+              select: {
+                id: true,
+                name: true
+              }
+            });
+        }));
 
         return new NextResponse(JSON.stringify({success: true, data }), {
             status: 200 // Define o status da resposta para 200 (OK)
